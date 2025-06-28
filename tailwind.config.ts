d39ef7add001eaf220c6ae31e014605321796f71
Tanmay jayant <JAYANTTANMAY96@gmail.com>
2025-06-28 10:35:01 +0000

    add more colors and variations to the gradients make them slower little

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -101,7 +101,7 @@ export default {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'wiggle': 'wiggle 0.4s ease-in-out infinite',
-        'animated-gradient': 'animated-gradient 15s ease infinite',
+        'animated-gradient': 'animated-gradient 30s ease infinite',
       },
     },
   },
